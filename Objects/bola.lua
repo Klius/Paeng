faefc58f4c42076dbc317efe26a60863ab563813
Klius <kliusescarlopini@gmail.com>
2017-03-18 11:23:55 +0100

    added basic color suport

--- Objects/bola.lua
+++ Objects/bola.lua
@@ -9,13 +9,18 @@ function Bola:new(x, y, radius)
     self.incrementSpeed = 15
     --A circle doesn't have a width or height. It has a radius.
     self.radius = radius
-    self.speedX = 350
+    self.speedX = self.defaultSpeed
     self.speed = self.defaultSpeed
     self.speedY = 0
-    self.colors = {{255,0,0,255}, {255,255,0,255},{98,150,255,255},{0,255,0,255}}
+    self.colors = {
+                    {255,255,255,255},{255,0,0,255}, {255,255,0,255},
+                    {98,150,255,255},{0,255,0,255}
+                  }
+    self.colorLevel = 1
 end
 
 function Bola:update(dt)
+  self:updateColorLevel()
   self.dt = dt
   self.x = self.x + self.speedX * dt
   self.y = self.y + self.speedY * dt
@@ -28,9 +33,23 @@ function Bola:update(dt)
   end
 end
 
+function Bola:updateColorLevel() 
+  
+  if self.speed > 400 and self.speed < 500 then
+    self.colorLevel = 2
+  elseif self.speed > 500 and self.speed < 600 then
+  elseif self.speed > 600 and self.speed < 700 then
+  else
+    
+  end 
+end
 function Bola:draw()
-    love.graphics.setColor(self.colors[4][1],self.colors[4][2],
-                            self.colors[4][3],self.colors[4][4])
+    love.graphics.setColor(
+                            self.colors[self.colorLevel][1],
+                            self.colors[self.colorLevel][2],
+                            self.colors[self.colorLevel][3],
+                            self.colors[self.colorLevel][4]
+                          )
     love.graphics.circle("line", self.x, self.y,self.radius)
     love.graphics.setColor(255,255,255,255)
 end
@@ -43,6 +62,7 @@ function Bola:reset()
   self.speed = self.defaultSpeed
   self.speedX = self.defaultSpeed
   self.speedY = 0
+  self.colorLevel = 1
 end
 function Bola:setSpeed(x,y)
   self.speedX = x
